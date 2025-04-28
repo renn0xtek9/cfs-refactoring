9059a728d809a6e95a51c17c2272dff12b6096c8
Maxime Haselbauer <maxime.haselbauer@googlemail.com>
2025-04-28 12:35:46 +0000

    Add original cFS
    legacy_build.sh: Script to build original cFS repository

---
+++ legacy_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+git submodule update --init --recursive
+
+cd current_cfs_repo/cFS/
+cp cfe/cmake/Makefile.sample Makefile
+cp -r cfe/cmake/sample_defs sample_defs
+make distclean
+make SIMULATION=native ENABLE_UNIT_TESTS=ON prep DESTDIR=/tmp/legacy_cfs/ 
+make 
+make install 
