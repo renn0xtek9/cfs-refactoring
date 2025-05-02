07b325b419e892e996d22cc6ce78cda48338cc7f
Maxime Haselbauer <maxime.haselbauer@googlemail.com>
2025-05-02 07:50:26 +0000

    process.sh: To build the new repository.

--- process.sh
+++ process.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 rm -rf build
 cmake --preset x86-Debug
-cmake --build --preset x86-Debug -j 20
-cmake --install build/build-x86-Debug-make/
+cmake --build --preset x86-Debug -- -j 20
+cmake --install build/build-x86-Debug-make/ 
