f5d6bea7afe62708f55459ca71ec642238a2079b
Maxime Haselbauer <renn0xtek9@laposte.net>
2025-05-11 06:40:46 +0000

    Fix unit test not working because of bad library linking

--- .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
+++ .devcontainer/setup_documentation_environment_on_ubuntu_22.sh
@@ -5,5 +5,5 @@ DEBIAN_FRONTEND=noninteractive
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 sudo apt-get update
-sudo apt-get install -y cmake git texlive-base texlive-science texlive-latex-extra biber doxygen graphviz makefile2graph plantuml
+sudo apt-get install -y git texlive-base texlive-science texlive-latex-extra biber doxygen graphviz makefile2graph plantuml
 python3 -m pip install -r requirements.txt
--- legacy_build.sh
+++ legacy_build.sh
@@ -8,5 +8,5 @@ cp cfe/cmake/Makefile.sample Makefile
 cp -r cfe/cmake/sample_defs sample_defs
 make distclean
 make SIMULATION=native ENABLE_UNIT_TESTS=ON prep DESTDIR=/tmp/legacy_cfs/ 
-make 
+make -j 20
 make install 
